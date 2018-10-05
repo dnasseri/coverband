b6bea05ea61c7c11a7546503bee7ab6b14a3d98c
Dean Nasseri <dnasseri10@gmail.com>
2018-10-05 16:25:32 -0400

    Reset coverage data on file change

--- test/unit/adapters_redis_store_test.rb
+++ test/unit/adapters_redis_store_test.rb
@@ -2,6 +2,16 @@
 
 require File.expand_path('../test_helper', File.dirname(__FILE__))
 
+class DummyChecksumGenerator
+  def initialize(checksum)
+    @checksum = checksum
+  end
+
+  def generate(file)
+    @checksum
+  end
+end
+
 class RedisTest < Test::Unit::TestCase
   BASE_KEY = Coverband::Adapters::RedisStore::BASE_KEY
 
@@ -13,21 +23,30 @@ class RedisTest < Test::Unit::TestCase
     @redis = Redis.new
     @redis.flushdb
     @store = Coverband::Adapters::RedisStore.new(@redis)
+    @store.checksum_generator = DummyChecksumGenerator.new('fakechecksum')
+  end
+
+  def test_invalidation_on_file_change
+    @store.checksum_generator = DummyChecksumGenerator.new('1234')
+    @store.save_report('lib/puppy.rb' => { 54 => 1, 55 => 0 })
+    @store.checksum_generator = DummyChecksumGenerator.new('234')
+    @store.save_report('lib/puppy.rb' => { 54 => 0, 55 => 1 })
+    assert_equal({ '54' => '0', '55' => '1' }, @store.covered_lines_for_file('lib/puppy.rb'))
   end
 
   def test_coverage
     @redis.sadd(BASE_KEY, 'dog.rb')
-    @store.send(:store_map, "#{BASE_KEY}.dog.rb", example_hash)
+    @store.send(:store_map, "#{BASE_KEY}.dog.rb", 'fakechecksum', example_hash)
     expected = { 'dog.rb' => example_hash }
     assert_equal expected, @store.coverage
   end
 
   def test_covered_lines_for_file
-    @store.send(:store_map, "#{BASE_KEY}.dog.rb", example_hash)
+    @store.send(:store_map, "#{BASE_KEY}.dog.rb", 'fakechecksum', example_hash)
     assert_equal [["1", "1"], ["2", "2"]], @store.covered_lines_for_file('dog.rb').sort
   end
 
-  def test_covered_lines_for_file__hash
+  def test_covered_lines_for_file_hash
     @redis.mapped_hmset("#{BASE_KEY}.dog.rb", '1' => 1, '2' => 2)
     @store = Coverband::Adapters::RedisStore.new(@redis)
     expected = [%w[1 1], %w[2 2]]
@@ -60,8 +79,10 @@ class RedisStoreTestV3Hash < RedisTest
     @redis = Redis.current.tap do |redis|
       redis.stubs(:sadd).with(anything, anything)
     end
+    @redis.flushdb
 
     @store = Coverband::Adapters::RedisStore.new(@redis)
+    @store.checksum_generator = DummyChecksumGenerator.new('fakechecksum')
   end
 
   test 'it stores the files into coverband' do
@@ -76,11 +97,11 @@ class RedisStoreTestV3Hash < RedisTest
   test 'it stores the file lines of the file app.rb' do
       @redis.expects(:mapped_hmset).with(
       "#{BASE_KEY}./Users/danmayer/projects/cover_band_server/app.rb",
-      '54' => 1, '55' => 2
+      '54' => 1, '55' => 2, 'checksum' => 'fakechecksum'
     )
     @redis.expects(:mapped_hmset).with(
       "#{BASE_KEY}./Users/danmayer/projects/cover_band_server/server.rb",
-      '5' => 1
+        '5' => 1, 'checksum' => 'fakechecksum'
     )
 
     @store.save_report(test_data)
